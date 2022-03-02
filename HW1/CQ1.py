82a77b444745316d6d3d86aa79d6710243b69358
Alireza Hosseini <sayyed.alireza.hosseini@gmail.com>
2022-03-02 20:47:39 +0330

    Add Q1 section 1

--- HW1/CQ1.py
+++ HW1/CQ1.py
@@ -1,1 +1,4 @@
-improt cv2 as cv
+import cv2 as cv
+imagePath=r"F:\\Semester 8\\IMP\\HW1\\ImageProcessing\\HW1\\campusdrive.png"
+img = cv.imread(imagePath)
+print("Image bit depth is : "+str(img.dtype)[-1])
